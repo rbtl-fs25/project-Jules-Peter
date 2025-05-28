4345ed45deca600951db685b77f0c12dc6881cd3
Jules-Peter <jupeter@ethz.ch>
2025-05-28 07:46:37 +0000

    Pushing to new folder in rbtl

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -5,10 +5,10 @@ library("tidyverse")
 library("googlesheets4")
 
 ###Reading the files from google drive 
-bacteria_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1ebVGUMhg8nP3Gw3SB31uc8O0oHBcpB5spuHSfflGB3Y/edit?usp=drive_link")
-experiment_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1MomeP7Ze-__ckRGYnVc0vEGAYguyDYRCSwLjdqlNdo0/edit?usp=drive_link")
-faeces_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/14D7pKH48NwMo57FGLHlRrPSq0tUb_e_OCZGtBfb3pfQ/edit?usp=drive_link")
-growth_speed_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1_HANDFM0bu00BEnb98vUIvHjrrEzJmIhUPWiKvE4Pw0/edit?usp=drive_link")
-inoculum_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1HOQQ1kHausy6wV6syHrEa-nih1btRHIXwTIr__fGrGA/edit?usp=drive_link")
+bacteria_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1McSNPNBHKzFB_rO39XHw6wTXRsa81-HBAM5tP5vicD8/edit?usp=drive_link")
+experiment_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1_b1ZWjh_HEOkDq6cXnWLg7AWZ4pPo1yyCzESEFA7G-I/edit?usp=drive_link")
+faeces_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/13989Xry9gG24fx4jVagXlzA72EJv_d2QsxogUl-8jQ0/edit?usp=drive_link")
+growth_speed_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1Tv4JjurbPrUQyOatEn1Jdb8Ze_biEM2_E-fWjo3UbWc/edit?usp=drive_link")
+inoculum_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1y0ZkTHQecAHVyPlak5AII1L1ZDVkS6Be4rwZk6LAJQg/edit?usp=drive_link")
 
 ###Writing the files in git hup directory follow up
